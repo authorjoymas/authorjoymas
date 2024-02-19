d9d45466090c20f62ae0bf786a8add43cf798ea4
ActuallyAdequate <actuallyadequate@gmail.com>
2024-02-19 20:16:05 +1100

    basic styles, 3 pages

---
+++ framework/nav.js
@@ -0,0 +1,15 @@
+fetch('components/nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#navbar");
+    let newelem = document.createElement("nav");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+
+    let a = document.getElementsByTagName('a');
+    for (let idx= 0; idx < a.length; ++idx){
+        if (a[idx].href == window.location.href) {
+            a[idx].firstChild.classList.add('active-link');
+        }
+    }
+})
