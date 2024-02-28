3913132aac8b67302b385b078c21cd64abc2c287
ActuallyAdequate <actuallyadequate@gmail.com>
2024-02-28 15:28:12 +1100

    fix localhost

--- web/framework/content.js
+++ web/framework/content.js
@@ -29,7 +29,7 @@ let newelem = document.createElement("main");
 oldelem.parentNode.replaceChild(newelem,oldelem);
 
 // assuming that its hosting on github or localhost,This is github pages specific and would need to be changed if moving to another platform
-let [url, mustParse] = (window.location.hostname == "localhost" || window.location.hostname == "") ? [window.location.origin,true] : ["https://api.github.com/repos/authorjoymas/authorjoymas/contents/", false];
+let [url, mustParse] = (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") ? [window.location.origin,true] : ["https://api.github.com/repos/authorjoymas/authorjoymas/contents/", false];
 
 fetch(`${url}${contentPath}`).then(res => {
     if( res.status != 200) {
